22be4fb1e0c1d8b5a369e9f70988b667df9a018d
Angs-human-B <angss.human.001@gmail.com>
2025-04-17 22:37:13 +0600

    BiblePage Ui made pixel-perfect and dynamic to different screen sizes

--- lib/Constants/icons.dart
+++ lib/Constants/icons.dart
@@ -1,2 +1,6 @@
 
 String searchIcon= "assets/icons/search.svg";
+String arrowDownIcon= "assets/icons/arrowDown.svg";
+String slashIcon= "assets/icons/slashIcon.svg";
+String arrowLeft= "assets/icons/arrowLeft.svg";
+String arrowRight= "assets/icons/arrowRight.svg";
